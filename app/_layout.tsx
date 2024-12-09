c367214a356b7c33540e70e7719575f46d207aa3
Leonard <leonard_posa@epam.com>
2024-12-09 11:40:53 +0100

    fixed navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,16 @@
 import {useFonts} from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import {useEffect, useState} from 'react';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
 import {Drawer} from "expo-router/drawer";
 import React from 'react';
-import LogoTitle from "@/components/LogoTitle";
 import {RootSiblingParent} from 'react-native-root-siblings';
 
-import {GestureHandlerRootView} from "react-native-gesture-handler";
-import {StyleSheet} from "react-native";
+import {GestureHandlerRootView, Pressable} from "react-native-gesture-handler";
+import {StyleSheet, Text} from "react-native";
 import {fetchLocations} from "@/services/locationService";
 import {usePushNotifications} from "@/notifications/usePushNoticiations";
+import {DrawerContentComponentProps, DrawerContentScrollView} from "@react-navigation/drawer";
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -53,59 +53,60 @@ export default function RootLayout() {
         return null;
     }
 
-    //TODO: Custom implementation of navigation items in case of bugs, add drawerContent={(props) => <CustomDrawerContent {...props}/>} to Drawer as a prop
-    /*const CustomDrawerContent = (props) => {
+    //TODO: Style this and maybe move it out as a seperate component?
+    const CustomDrawerContent = (props: DrawerContentComponentProps) => {
         return (
             <DrawerContentScrollView {...props} contentContainerStyle={styles.drawerContainer}>
-                <TouchableOpacity
+                <Pressable
                     style={styles.drawerButton}
                     onPress={() => props.navigation.navigate('index')}
                 >
                     <Text style={styles.drawerButtonText}>Početna</Text>
-                </TouchableOpacity>
+                </Pressable>
 
-                <TouchableOpacity
+                <Pressable
                     style={styles.drawerButton}
                     onPress={() => props.navigation.navigate('MoreAboutActions')}
                 >
-                    <Text style={styles.drawerButtonText}>Novosti</Text>
-                </TouchableOpacity>
+                    < Text style={styles.drawerButtonText}>Novosti</Text>
+                </Pressable>
 
-                <TouchableOpacity
+                <Pressable
                     style={styles.drawerButton}
                     onPress={() => props.navigation.navigate('EmailForm')}
                 >
                     <Text style={styles.drawerButtonText}>Prijavi problem</Text>
-                </TouchableOpacity>
+                </Pressable>
 
-                <TouchableOpacity
+                <Pressable
                     style={styles.drawerButton}
                     onPress={() => props.navigation.navigate('Map')}
                 >
                     <Text style={styles.drawerButtonText}>Mapa</Text>
-                </TouchableOpacity>
+                </Pressable>
 
-                <TouchableOpacity
+                <Pressable
                     style={styles.drawerButton}
                     onPress={() => props.navigation.navigate('TermsConditions')}
                 >
                     <Text style={styles.drawerButtonText}>Uslovi korišćenja</Text>
-                </TouchableOpacity>
+                </Pressable>
 
-                <TouchableOpacity
+                <Pressable
                     style={styles.drawerButton}
                     onPress={() => props.navigation.navigate('AboutUs')}
                 >
                     <Text style={styles.drawerButtonText}>O nama</Text>
-                </TouchableOpacity>
+                </Pressable>
             </DrawerContentScrollView>
         );
-    };*/
+    };
 
     return (
         <RootSiblingParent>
             <GestureHandlerRootView style={{flex: 1}}>
                 <Drawer
+                    drawerContent={(props) => <CustomDrawerContent {...props}/>}
                     screenOptions={{
                         headerTitleAlign: 'center',
                         drawerStyle: {
@@ -115,52 +116,6 @@ export default function RootLayout() {
                         drawerActiveTintColor: 'white',
                         drawerInactiveTintColor: 'white',
                     }}>
-                    <Drawer.Screen
-                        name="index"
-                        options={{
-                            headerTitle: () => <LogoTitle/>,
-                            drawerLabel: "Početna",
-                        }}
-                    />
-
-                    <Drawer.Screen
-                        name="MoreAboutActions"
-                        options={{
-                            headerTitle: () => <LogoTitle/>,
-                            drawerLabel: "Novosti",
-                        }}
-                    />
-
-                    <Drawer.Screen
-                        name="EmailForm"
-                        options={{
-                            headerTitle: () => <LogoTitle/>,
-                            drawerLabel: "Prijavi problem",
-                        }}
-                    />
-
-                    <Drawer.Screen
-                        name="Map"
-                        options={{
-                            headerTitle: () => <LogoTitle/>,
-                            drawerLabel: "Mapa",
-                        }}
-                    />
-
-                    <Drawer.Screen
-                        name="TermsConditions"
-                        options={{
-                            headerTitle: () => <LogoTitle/>,
-                            drawerLabel: "Uslovi korišćenja",
-                        }}
-                    />
-                    <Drawer.Screen
-                        name="AboutUs"
-                        options={{
-                            headerTitle: () => <LogoTitle/>,
-                            drawerLabel: "O nama",
-                        }}
-                    />
 
                 </Drawer>
             </GestureHandlerRootView>
