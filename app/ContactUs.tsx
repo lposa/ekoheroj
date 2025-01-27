6ee25e2cd79c6d572fe5d83d7aa58676fed3461c
Dragana Zabaljac <dragana_zabaljac@epam.com>
2025-01-27 11:27:29 +0100

    removed email from contact us as we're sending from email app

--- app/ContactUs.tsx
+++ app/ContactUs.tsx
@@ -15,7 +15,6 @@ import * as MailComposer from 'expo-mail-composer';
 import Toast from 'react-native-root-toast';
 
 const ContactUs = () => {
-    const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
     const [isAvailable, setIsAvailable] = useState(false);
 
@@ -45,7 +44,8 @@ const ContactUs = () => {
         if (isAvailable) {
             MailComposer.composeAsync({
                 subject: 'Kontakt',
-                body: `<h3>Poruka: ${message}</h3>`,
+                body: `<h3>Poruka iz kontakt forme: </h3>
+                <h3>${message}</h3>`,
                 ccRecipients: [''],
                 bccRecipients: [''],
                 isHtml: true,
@@ -70,13 +70,6 @@ const ContactUs = () => {
                 </View>
                 <View>
                     <TextInput
-                        placeholder="Email"
-                        placeholderTextColor={'#999999'}
-                        onChangeText={text => setEmail(text)}
-                        style={styles.input}
-                        value={email}
-                    />
-                    <TextInput
                         placeholder="Poruka"
                         multiline={true}
                         onChangeText={text => setMessage(text)}
@@ -98,7 +91,6 @@ const ContactUs = () => {
 const styles = StyleSheet.create({
     scrollView: {
         display: 'flex',
-//         alignItems: 'center',
         paddingRight: 20,
         paddingLeft: 20,
     },
@@ -113,10 +105,6 @@ const styles = StyleSheet.create({
         fontFamily: 'DongleRegular',
         backgroundColor: '#fff',
         color: '#00405C',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
         shadowColor: '#000000',
         shadowOpacity: 0.6,
         shadowRadius: 4.65,
@@ -127,11 +115,6 @@ const styles = StyleSheet.create({
     multilineInput: {
         textAlignVertical: 'top',
         height: 200
-    },
-    buttonText: {
-        fontSize: 24,
-        fontFamily: 'DongleRegular',
-        color: '#999999',
     },
     submitBtn: {
         display: 'flex',
@@ -150,52 +133,6 @@ const styles = StyleSheet.create({
         fontSize: 26,
         fontFamily: 'DongleBold',
     },
-    buttonsContainer: {
-        display: 'flex',
-        marginRight: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    cameraBtn: {
-        backgroundColor: '#fff',
-        borderRadius: 8,
-        padding: 10,
-        marginBottom: 20,
-        shadowColor: '#000000',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.6,
-        shadowRadius: 4.65,
-        elevation: 2,
-    },
-    btnContainer: {
-        display: 'flex',
-        width: '80%',
-        padding: 10,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    imageNamesContainer: {
-        backgroundColor: '#FF6600',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        color: '#fff',
-        marginBottom: 10,
-        borderRadius: 30,
-        marginLeft: 10,
-        paddingVertical: 5,
-        paddingHorizontal: 15,
-    },
-    imageNames: {
-        width: '80%',
-        color: '#fff',
-        fontFamily: 'DongleRegular',
-        fontSize: 22,
-    },
     titleContainer: {
         flexDirection: 'row',
         alignSelf: 'flex-start',
@@ -208,41 +145,6 @@ const styles = StyleSheet.create({
         fontWeight: '700',
         marginLeft: 10,
     },
-    camera: {
-        flex: 1,
-        position: 'relative',
-        zIndex: 999,
-        width: '100%',
-        height: "100%",
-        alignItems: 'center',
-        justifyContent: 'flex-end'
-    },
-    cameraTakePic: {
-        position: 'relative',
-        width: 100,
-        height: 50,
-        borderRadius: 5,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        marginBottom: 20,
-    },
-    cameraText: {
-        textAlign: 'center',
-        fontSize: 20,
-        textTransform: 'uppercase',
-        fontFamily: "DongleBold"
-    },
-    closeBtn: {
-        position: 'absolute',
-        top: 20,
-        right: 30,
-        backgroundColor: 'white',
-        padding: 10,
-        borderRadius: 5,
-        textTransform: 'uppercase',
-        fontFamily: "DongleBold",
-        height: 20
-    }
 });
 
 
